a4bd0eee2b31bf4f535f5402cf8a6ca9d7091beb
Linar Khilazhev <l.khilajev@pologames.org>
2021-02-01 17:50:14 +0300

    Base tank moving and rotation

--- Assets/BaseController.cs
+++ Assets/BaseController.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float movingSpeed;
 
-    // Update is called once per frame
+    [HideInInspector] public float rotationSpeed = 100f;
+
+
     void Update()
     {
-        
+        if (Input.GetButton("Vertical"))
+        {
+            var verticalValue = Input.GetAxis("Vertical") > 0 ? 1 : -1;
+
+            transform.position += (verticalValue * transform.forward) * (movingSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+            var rotationValue = Input.GetAxis("Horizontal");
+
+            transform.Rotate(rotationSpeed * rotationValue * Vector3.up * Time.deltaTime);
+        }
     }
 }
