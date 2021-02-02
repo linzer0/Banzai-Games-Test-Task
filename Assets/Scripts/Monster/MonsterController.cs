f67de47bb00eecf2f24b55cb8963c5d6ceec28df
Linar Khilazhev <l.khilajev@pologames.org>
2021-02-02 15:20:44 +0300

    Monster chase logic ready

---
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -0,0 +1,27 @@
+﻿using Components;
+using UnityEngine;
+
+namespace Monster
+{
+    public class MonsterController : MonoBehaviour
+    {
+        [HideInInspector] public GameObject Target;
+
+        private DataComponent DataComponent;
+
+        void Start()
+        {
+            DataComponent = gameObject.GetComponent<DataComponent>();
+        }
+
+        void Update()
+        {
+            if (Target != null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, Target.transform.position,
+                    DataComponent.Speed * Time.deltaTime);
+                transform.LookAt(Target.transform.position);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Monster/MonsterCreator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monster
+{
+    public class MonsterCreator : MonoBehaviour
+    {
+        public int MonsterAmount = 10;
+        
+        [SerializeField] private List<GameObject> MonsterPrefabs;
+        [SerializeField] private GameObject ChaseTarget;
+
+        void Start()
+        {
+            CreateMonsters(MonsterAmount);
+        }
+
+        void CreateMonster(GameObject prefab, Vector3 spawnPosition)
+        {
+            var monsterObject = Instantiate(prefab, spawnPosition, Quaternion.identity);
+            
+            var healthController = monsterObject.GetComponent<HealthController>();
+            healthController.OnDeath += OnMonsterDestroy;
+            
+            var monsterController = monsterObject.GetComponent<MonsterController>();
+            monsterController.Target = ChaseTarget;
+        }
+
+        private void CreateMonsters(int amount = 1)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                var monsterPosition = GetSpawnPosition();
+                var monsterPrefab = MonsterPrefabs[Random.Range(0, MonsterPrefabs.Count)];
+                
+                CreateMonster(monsterPrefab, monsterPosition);
+            }
+        }
+
+        private void OnMonsterDestroy()
+        {
+           CreateMonsters(); 
+        }
+
+        private Vector3 GetSpawnPosition()
+        {
+            var position = Vector3.one;
+            position.x += Random.Range(0, 20);
+            position.y += Random.Range(0, 20);
+            position.z += Random.Range(0, 20);
+            return position;
+        }
+    }
+}
